469b7bea9fa71529cc4968cc9689ea0f26de4ee6
Luiggyp26 <luiggymamani@gmail.com>
2022-06-08 10:03:20 -0400

    Added cab class in model and cabView class in View

---
+++ src/View/CabView.java
@@ -0,0 +1,18 @@
+package View;
+
+import java.util.ArrayList;
+
+public class CabView {
+
+    private void showTaxisList(ArrayList<Cab> taxisList){
+        for (Cab cab : taxisList) {
+            int licensePlate = cab.getLicensePlate();
+            boolean availability = cab.getAvailability();
+        }
+    }
+
+    public void showTaxisList(int licensePlate, boolean availability){
+        System.out.println("\t" + "License Plate: " + licensePlate);
+        System.out.println("\t" + "Availability: " + availability + "\n");
+    }
+}
