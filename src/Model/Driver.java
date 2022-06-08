a4890ef92e51db1b538213ba8301923553e744dc
Eduard herrera <eduardjhonnherrera@gmail.com>
2022-06-08 14:55:38 -0400

    add DriverView
    . Add Driver and DriverView files

--- src/Model/Driver.java
+++ src/Model/Driver.java
@@ -1,5 +1,47 @@
 package Model;
 
 public class Driver {
-//prueba
+    private String name;
+    private int ci;
+    private int phone;
+    private int age;
+
+    public Driver(String name, int ci, int phone, int age) {
+        this.name = name;
+        this.ci = ci;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public void setCi(int ci) {
+        this.ci = ci;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ src/View/DriverView.java
@@ -0,0 +1,11 @@
+package View;
+
+public class DriverView {
+    public void printDriverView(String name, int ci, int phone, int age)
+    {
+        System.out.println("Name: " + name);
+        System.out.println("CI: : " + ci);
+        System.out.println("Phone: " + phone);
+        System.out.println("Age: " + age);
+    }
+}
